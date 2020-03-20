eeb6290679ad73745aee73c7d1323b65f87dce7c
AngularNinjaAvenger <osayibishop@gmail.com>
2020-03-20 01:59:32 +0100

    working on news page

--- covid/lib/views/news.dart
+++ covid/lib/views/news.dart
@@ -6,7 +6,6 @@ class News extends StatefulWidget {
   @override
   _NewsState createState() => _NewsState();
 }
-
 class _NewsState extends State<News> {
   @override
   Widget build(BuildContext context) {
