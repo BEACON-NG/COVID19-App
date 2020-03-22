024ece3c735cb17ee93a8cfd2928f3f06d60aa69
AngularNinjaAvenger <osayibishop@gmail.com>
2020-03-22 03:56:08 +0100

    fixing render fix eror

--- covid/lib/views/selected_news.dart
+++ covid/lib/views/selected_news.dart
@@ -10,10 +10,78 @@ class SelectedNews extends StatefulWidget {
 }
 
 class _SelectedNewsState extends State<SelectedNews> {
-  bool _pinned = true;
-  bool _snap = false;
+  bool _pinned = false;
+  bool _snap = true;
   bool _floating = false;
+  Color color = Color.fromRGBO(36, 37, 130, 1);
 
+  Flex social(){
+    return Flex(
+      direction: Axis.horizontal,
+      children: <Widget>[
+        Icon(
+              Icons.headset,
+              size:32,
+              color:color
+            ),
+            SizedBox(width:3),
+            Text(
+              "20",
+              style:TextStyle(
+                fontSize: 14,
+                fontWeight:FontWeight.w500,
+                color:color
+              )
+            )
+      ],
+    );
+  }
+
+  Expanded left(){
+    return Expanded(
+      child:Container(
+        child:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            social(),
+            social(),
+          ])
+          )
+    );
+  }
+  Container rightIcons(){
+    return Container(
+      child:Flex(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Icon(Icons.share,color:color,size:32),
+          SizedBox(width:8),
+          Icon(Icons.save,color:color,size:32),
+        ],
+      )
+    );
+  }
+  Expanded right(){
+    return Expanded(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                rightIcons()
+              ]),
+      ),
+    );
+  }
+
+  Container newsText(String text){
+    return Container(
+      margin:EdgeInsets.symmetric(vertical:10,horizontal:10),
+      child:Text(
+          text,
+          textAlign: TextAlign.start,
+          textDirection: TextDirection.ltr)
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,21 +90,54 @@ class _SelectedNewsState extends State<SelectedNews> {
         body: CustomScrollView(
           slivers:<Widget>[
               SliverAppBar(
+              backgroundColor: Colors.white,
               pinned: this._pinned,
               snap: this._snap,
               floating: this._floating,
-              expandedHeight: 200.0,
+              expandedHeight: 250.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: Text("news title"),
+                title:  Text(
+                    "Lattest news corona virus don catch 10 new people o",
+                    style:TextStyle(
+                      color: Colors.black,
+                      fontSize: 14
+                    )),
                 background: Image.asset(
                   'assets/images/coro.png',
                   fit: BoxFit.fill,
                 ),
               ),
             ),
-            SliverFillRemaining(
-              child: Center(child: Text("Hello")),
-            )
+            SliverList(
+              delegate: SliverChildListDelegate(
+                  [
+
+                    Container(
+                      padding:EdgeInsets.all(10),
+                      width:MediaQuery.of(context).size.width,
+                      child: Row(
+                        children: <Widget>[
+                          left(),
+                          right(),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children:<Widget>[
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+                          newsText("so fa this is the lattest news about corna virus ,the virus just they spread every whee oo coroco leave us a lone oo"),
+      ]
+                    )
+                  ]
+                )
+              )
           ]
         )
       ),
